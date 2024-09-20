67df54419966b1cd2a2850a84cc609f89c7f787e
Thaynara yasmin silva carlo <thaynara.carlo@alunoescolassociais.org.br>
2024-09-20 08:13:33 -0300

    função para limpar formulário

---
+++ for.js
@@ -0,0 +1,9 @@
+'use strict'; // Modo restrito
+// Este modo faz com que o javascript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação 
+/* Consumo de API - https://viacep.com.br/ */
+
+// Função para limpar formulário 
+const limparFormulario = () =>{
+    document.getElementById('Logadouro').value = '';
+
+}
